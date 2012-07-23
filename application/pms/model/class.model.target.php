ce1dc52ba4bae2dcd3549b8deb51322ae134b8cc
wing075@gmail.com <wing075@gmail.com@9993907b-54c4-7198-f56c-1cf6e07f7ffb>
2012-07-23 16:22:24 +0000

    单任务添加功能 单任务内页BUG修复 目标地址模块

--- application/pms/model/class.model.target.php
+++ application/pms/model/class.model.target.php
@@ -7,4 +7,33 @@ class ModelTarget extends ModelCommon
 	{
 		parent::__construct();
 	}
+
+	public function contactToId($contact)
+	{
+		$condition = array();
+		$condition[] = array('contact' => array('eq', $contact));
+		$id = $this->getOne($condition, 'id');
+		if($id < 1)
+		{
+			$type = $this->contactType($contact);
+			if($type > 0)
+			{
+				$id = $this->insert(array('contact' => $contact, 'type' => $type, 'is_disable' => 0));
+			}
+		}
+		return $id;
+	}
+
+	public function contactType($contact)
+	{
+		if(preg_match('/^[\w\._]+@(?:[\w-]+\.)+\w{2,4}$/', $contact))
+		{
+			return 1;
+		}
+		else if(preg_match('/^1[358]\d{9}$/', $contact))
+		{
+			return 2;
+		}
+		return 0;
+	}
 }
--- application/pms/model/class.model.tasksingle.php
+++ application/pms/model/class.model.tasksingle.php
@@ -51,6 +51,146 @@ class ModelTaskSingle extends ModelCommon
 		return $data;
 	}
 
+	public function add($data)
+	{
+		// 目标地址检查
+		$targetObj = Factory::getModel('target');
+		$data['target_id'] = $targetObj->contactToId($data['target_contact']);
+		if($data['target_id'] < 1)
+		{
+			return array('state' => false, 'message' => '目标地址错误。');
+		}
+		$target_type = $targetObj->contactType($data['target_contact']);
+		unset($data['target_contact']);
+
+		// 通道检查
+		$channelObj = Factory::getModel('channel');
+		$channel = $channelObj->getObject(array(array('id' => array('eq', $data['channel_id']))));
+		if($channel['type'] != $target_type)
+		{
+			return array('state' => false, 'message' => '通道类型与目标地址类型不符。');
+		}
+
+		// 标题检查
+		$data['title'] = trim($data['title']);
+		if($target_type == CHANNEL_TYPE_EMAIL && empty($data['title']))
+		{
+			return array('state' => false, 'message' => '邮件标题不能为空。');
+		}
+
+		// 内容检查
+		$data['content'] = trim($data['content']);
+		if(empty($data['content']))
+		{
+			return array('state' => false, 'message' => '发送内容不能为空。');
+		}
+
+		// 创建人检查
+		if(!$this->getOne(array(array('id' => array('eq', $data['user_id']))), 'COUNT(*)', 'user'))
+		{
+			return array('state' => false, 'message' => '创建人不存在。');
+		}
+
+		// 计划发送时间
+		$data['plan_send_time'] = strtotime($data['plan_send_time']);
+		if($data['plan_send_time'] < time())
+		{
+			$data['plan_send_time'] = time();
+		}
+
+		// 默认参数
+		$data['create_time'] = time();
+		$data['send_state'] = 1;
+		$data['send_time'] = 0;
+		$data['page_view'] = 0;
+		$data['first_read_time'] = 0;
+		$data['last_read_time'] = 0;
+
+		$state = parent::insert($data);
+		$state && $this->record($state, LOG_DATA_TABLE_TASKSINGLE, LOG_OPERATION_TYPE_INSERT);
+
+		$message = $state ? $state : '保存失败。';
+		return array('state' => $state, 'message' => $message);
+	}
+
+	public function edit($id, $data)
+	{// 通道过滤
+	/*	foreach($data as $k => $v)
+		{
+			switch($k)
+			{
+				case 'account':
+					$data[$k] = trim($v);
+					if(empty($data[$k]))
+					{
+						return array('state' => false, 'message' => '账号不能为空。');
+					}
+					else if($this->getOne(array(array('account' => array('eq', $data[$k]))), 'COUNT(*)') === false)
+					{
+						return array('state' => false, 'message' => '账号已经存在。');
+					}
+					break;
+				case 'password':
+					$data[$k] = trim($v);
+					if(empty($data[$k]))
+					{
+						unset($data[$k]);
+					}
+					else
+					{
+						$data[$k] = md5($data[$k]);
+					}
+					break;
+				case 'power':
+					$power_list = array_keys($GLOBALS['CONFIG']['POWER']);
+					if(is_array($data[$k]) && !empty($data[$k]))
+					{
+						foreach($data[$k] as $_k => $_v)
+						{
+							if(!in_array($_v, $power_list))
+							{
+								unset($data[$k][$_k]);
+							}
+						}
+					}
+					$data[$k] = (is_array($data[$k]) && count($data[$k]) > 0) ? implode(';', $data[$k]) : '';
+					break;
+				case 'channel':
+					if(is_array($data[$k]) && !empty($data[$k]))
+					{
+						$channelObj = Factory::getModel('channel');
+						$channels = array_keys($channelObj->getAllPairs());
+						foreach($data[$k] as $_k => $_v)
+						{
+							if(!in_array($_v, $channels))
+							{
+								unset($data[$k][$_k]);
+							}
+						}
+					}
+					$data[$k] = (is_array($data[$k]) && count($data[$k]) > 0) ? implode(';', $data[$k]) : '';
+					break;
+				case 'is_disable':
+					$data[$k] = intval($data[$k]) > 0 ? 1 : 0;
+					break;
+				case 'tasksingle_limit_day':
+					$data[$k] = intval($data[$k]) >= 0 ? $data[$k] : 1;
+					break;
+				default:
+					unset($data[$k]);
+			}
+		}
+
+		$condition = array();
+		$condition[] = array('id' => array('eq', $id));
+		$state = parent::update($condition, $data);
+		$state = ($state !== false);
+		$state && $this->record($id, LOG_DATA_TABLE_USER, LOG_OPERATION_TYPE_UPDATE);
+
+		$message = $state ? '保存成功。' : '保存失败。';
+		return array('state' => $state, 'message' => $message);*/
+	}
+
 	public function cancel($id)
 	{
 		$condition = array();
