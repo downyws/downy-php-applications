6b85fd8d26cd28b168144bac1b8da1d36ee1aea9
wing075@gmail.com <wing075@gmail.com@9993907b-54c4-7198-f56c-1cf6e07f7ffb>
2012-07-28 08:27:44 +0000

    文件编码识别

--- trunk/library/filehelper/library.filehelper.php
+++ trunk/library/filehelper/library.filehelper.php
@@ -15,4 +15,28 @@ class FileHelper
 		closedir($handle);
 		return $size;
 	}
+
+	public function getEncode($file)
+	{
+		// 	mb_convert_encoding
+		$string = file_get_contents($file);
+
+		if($string === @iconv('UTF-8', 'ASCII', @iconv('ASCII', 'UTF-8', $string)))
+		{
+			return 'ASCII';
+		}
+		else if($string === @iconv('UTF-8', 'GB2312', @iconv('GB2312', 'UTF-8', $string)))
+		{
+			return 'GB2312';
+		}
+		else if(chr(239) . chr(187) . chr(191) == substr($string, 0, 3))
+		{
+			return 'UTF-8 BOM';
+		}
+		else if($string === @iconv('UTF-8', 'UTF-8', @iconv('UTF-8', 'UTF-8', $string)))
+		{
+			return 'UTF-8';
+		}
+		return 'UNKNOW';
+	}
 }
