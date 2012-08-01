722b3fbca17419ea539b763169546fc8888febab
wing075@gmail.com <wing075@gmail.com@9993907b-54c4-7198-f56c-1cf6e07f7ffb>
2012-08-01 15:56:13 +0000

    通道优化

--- trunk/application/pms_oa/channel/class.channel_1.php
+++ trunk/application/pms_oa/channel/class.channel_1.php
@@ -14,56 +14,17 @@ class Channel_1 extends Channel
 		$this->task_count['multi'] = 50;
 
 		// 初始化邮件
+		$this->type = CHANNEL_TYPE_EMAIL;
 		Factory::loadLibrary('phpmailer');
-		$this->mail = new PHPMailer(true);
-		$this->mail->IsSMTP();
-		$this->mail->SMTPAuth = true;
-		$this->mail->Port = 25;
-		$this->mail->Host = "smtp.qq.com";
-		$this->mail->Username = "service@wing075.com";
-		$this->mail->Password = "myj123456";
-		$this->mail->From = "service@wing075.com";
-		$this->mail->FromName = "service";
-		$this->mail->IsHTML(true);
-	}
-
-	public function run()
-	{
-		foreach($this->task_obj as $k => $v)
-		{
-			$result = $this->task_obj[$k]->taskReceive($this->channel_id, $this->task_count[$k]);
-			if(!$result['state'])
-			{
-				return $result;
-			}
-			$list = $result['data'];
-
-			$this->task_count[$k] = count($list);
-			if(is_array($list) && count($list) > 0)
-			{
-				$result = array();
-				foreach($list as $_v)
-				{
-					try
-					{
-						$this->mail->AddAddress($_v['contact']);
-						$this->mail->Subject = $_v['title'];
-						$this->mail->MsgHTML($_v['content']);
-						$state = $this->mail->Send();
-						$result[$_v['id']] = $state;
-						$this->run_count[$k][$state ? 'success' : 'failed']++;
-					}
-					catch(phpmailerException $e)
-					{
-						$result[$_v['id']] = false;
-						$this->run_count[$k]['failed']++;
-						$this->errors[] = $k . '_' . $_v['id'] . ':' . $e->errorMessage();
-					}
-				}
-				$this->task_obj[$k]->taskSubmit($result);
-			}
-			$this->task_obj[$k]->taskReflash($this->channel_id);
-		}
-		return array('state' => true, 'message' => '发送信息 ' . ($this->task_count['single'] + $this->task_count['multi']) . ' 条，耗时 ' . number_format(microtime(true) - $this->run_time['start'], 4, '.', '') . ' 秒。');
+		$this->type_obj = new PHPMailer(true);
+		$this->type_obj->IsSMTP();
+		$this->type_obj->SMTPAuth = true;
+		$this->type_obj->Port = 25;
+		$this->type_obj->Host = "";
+		$this->type_obj->Username = "";
+		$this->type_obj->Password = "";
+		$this->type_obj->From = "";
+		$this->type_obj->FromName = "";
+		$this->type_obj->IsHTML(true);
 	}
 }
