77dcc7eb825b1b7aadea2184802c38df1ed62f38
wing075@gmail.com <wing075@gmail.com@9993907b-54c4-7198-f56c-1cf6e07f7ffb>
2013-01-15 16:39:30 +0000

    dyDialog

--- trunk/application/pass/web/js/common.js
+++ trunk/application/pass/web/js/common.js
@@ -223,5 +223,163 @@ $.fn.extend({
 			obj.removeClass("msgerr");
 			$(this).removeClass("error");
 		});
+	},
+	// 对话框
+	dyDialog: function(config){
+		// 参数格式化
+		if(typeof(config.content) == "undefined") config.content = "";
+		if(typeof(config.bgStyle) == "undefined") config.bgStyle = {};
+		if(typeof(config.locate.type) == "undefined") config.locate.type = "window";
+		if(typeof(config.locate.target.object) == "undefined") config.locate.target.object = false;
+		if(typeof(config.locate.target.origin) == "undefined") config.locate.target.origin = "lt";
+		if(typeof(config.locate.x) == "undefined") config.locate.x = 0;
+		if(typeof(config.locate.y) == "undefined") config.locate.y = 0;
+		if(typeof(config.locate.origin) == "undefined") config.locate.origin = "lt";
+		if(typeof(config.onEvent.open) == "undefined") config.onEvent.open = function(){};
+		if(typeof(config.onEvent.close) == "undefined") config.onEvent.close = function(){};
+		if(typeof(config.onEvent.resize) == "undefined") config.onEvent.resize = function(){};
+		if(typeof(config.close.bgBtn) == "undefined") config.close.bgBtn = false;
+		if(typeof(config.close.className) == "undefined") config.close.className = false;
+
+		// 事件
+		var H_Close = function(){
+			// 解绑
+			$(window).unbind('resize', H_WindowResize);
+			config.close.bgBtn && $("#" + id + "-bg").unbind('click', H_Close);
+			config.close.className && $("#" + id + "-ft ." + config.close.className).unbind('click', H_Close);
+			// 销毁对象
+			$("#" + id + "-bg, #" + id + "-ft").fadeOut(300, function(){
+				$(this).remove();
+			});
+			// 关闭回调
+			config.onEvent.close();
+		}
+		var H_WindowResize = function(){
+			$("#" + id + "-bg").css({"width": "0px", "height": "0px"});
+			$("#" + id + "-ft").css({"top": 0, "left": 0});
+
+			if(config.locate.type == "window"){
+				F_LocateWindow();
+			}else if(config.locate.type == "page"){
+				F_LocatePage();
+			}else if(config.locate.type == "tag"){
+				F_LocateTag();
+			}
+			var w = $(document).width(), h = $(document).height();
+			$("#" + id + "-bg").css({"width": w + "px", "height": h + "px"});
+
+			config.onEvent.resize();
+		};
+		// 窗口类型定位
+		var F_LocateWindow = function(){
+			var ft_w = $("#" + id + "-ft").innerWidth(), ft_h = $("#" + id + "-ft").innerHeight();
+			var w_w = $(window).width(), w_h = $(window).height();
+			var x = 0, y = 0;
+
+			if(c_x == "left") x = 0;
+			else if(c_x == "middle") x = w_w / 2;
+			else if(c_x == "right") x = w_w;
+			else x = c_x;
+
+			if(c_y == "top") y = 0;
+			else if(c_y == "middle") y = w_h / 2;
+			else if(c_y == "bottom") y = w_h;
+			else y = c_y;
+
+			var xy = F_Offset(x, y, c_o, ft_w, ft_h);
+			$("#" + id + "-ft").css("left", xy.x);
+			$("#" + id + "-ft").css("top", xy.y);
+		};
+		// 页面类型定位
+		var F_LocatePage = function(){
+			var ft_w = $("#" + id + "-ft").innerWidth(), ft_h = $("#" + id + "-ft").innerHeight();
+			var d_w = $(document).width(), d_h = $(document).height();
+			var x = 0, y = 0;
+
+			if(c_x == "left") x = 0;
+			else if(c_x == "middle") x = d_w / 2;
+			else if(c_x == "right") x = d_w;
+			else x = c_x;
+
+			if(c_y == "top") y = 0;
+			else if(c_y == "middle") y = d_h / 2;
+			else if(c_y == "bottom") y = d_h;
+			else y = c_y;
+
+			var xy = F_Offset(x, y, c_o, ft_w, ft_h);
+			$("#" + id + "-ft").css("left", xy.x);
+			$("#" + id + "-ft").css("top", xy.y);
+		};
+		// 元素类型定位
+		var F_LocateTag = function(){
+			var ft_w = $("#" + id + "-ft").innerWidth(), ft_h = $("#" + id + "-ft").innerHeight();
+			var t_w = config.locate.target.object.innerWidth(), t_h = config.locate.target.object.innerHeight();
+			var t_x = config.locate.target.object.offset().left, t_y = config.locate.target.object.offset().top;
+			var x = 0, y = 0;
+
+			if(c_x == "left") x = t_x;
+			else if(c_x == "middle") x = t_x + t_w / 2;
+			else if(c_x == "right") x = t_x + t_w;
+			else switch(config.locate.target.origin){
+				case "mt": case "mm": case "mb": x = t_x + t_w / 2 + c_x; break;
+				case "rt": case "rm": case "rb": x = t_x + t_w + c_x; break;
+				case "lt": case "lm": case "lb": default: x = t_x + c_x; break;
+			}
+
+			if(c_y == "top") y = t_y;
+			else if(c_y == "middle") y = t_y + t_h / 2;
+			else if(c_y == "bottom") y = t_y + t_h;
+			else switch(config.locate.target.origin){
+				case "lm": case "mm": case "rm": y = t_y + t_h / 2 + c_y; break;
+				case "lb": case "mb": case "rb": y = t_y + t_h + c_y; break;
+				case "lt": case "mt": case "rt": default: y = t_y + c_y; break;
+			}
+
+			var xy = F_Offset(x, y, c_o, ft_w, ft_h);
+			$("#" + id + "-ft").css("left", xy.x);
+			$("#" + id + "-ft").css("top", xy.y);
+		};
+		// 对齐点偏移
+		var F_Offset = function(left, top, origin, width, height){
+			switch(origin){
+				case "lm": return {x: left, y: top - height / 2}; break;
+				case "lb": return {x: left, y: top - height}; break;
+				case "mt": return {x: left - width / 2, y: top}; break;
+				case "mm": return {x: left - width / 2, y: top - height / 2}; break;
+				case "mb": return {x: left - width / 2, y: top - height}; break;
+				case "rt": return {x: left - width, y: top}; break;
+				case "rm": return {x: left - width, y: top - height / 2}; break;
+				case "rb": return {x: left - width, y: top - height}; break;
+				case "lt": default: return {x: left, y: top}; break;
+			}
+		};
+
+		// 创建对象
+		var id = "dy-dialog-" + parseInt(Math.random() * 1000);
+		$("body").append("<div id='" + id + "-bg'></div>");
+		$("#" + id + "-bg").css({"top":"0", "left":"0", "position":"absolute", "z-index":"999", "background-color":"#DDDDDD", "opacity":"0.6"});
+		$("#" + id + "-bg").css(config.bgStyle);
+
+		// 定位
+		var c_x = config.locate.x * 1, c_y = config.locate.y * 1, c_o = config.locate.origin;
+		$("body").append("<div id='" + id + "-ft'>" + config.content + "</div>");
+		if(config.locate.type == "window"){
+			$("#" + id + "-ft").css({"position":"fixed", "z-index":"1000"});
+		}else if(config.locate.type == "page"){
+			$("#" + id + "-ft").css({"position":"absolute", "z-index":"1000"});
+		}else if(config.locate.type == "tag"){
+			$("#" + id + "-ft").css({"position":"absolute", "z-index":"1000"});
+		}
+
+		// 绑定
+		config.close.bgBtn && $("#" + id + "-bg").bind('click', H_Close);
+		config.close.className && $("#" + id + "-ft ." + config.close.className).bind('click', H_Close);
+		$(window).bind('resize', H_WindowResize);
+
+		// 展现
+		H_WindowResize();
+		$("#" + id + "-bg, #" + id + "-ft").css("display", "none").fadeIn(300);
+		// 回调
+		config.onEvent.open();
 	}
 });
